6d0d82666aea97fb2d8955d835ffbe848827ce26
Marie-Jade Menni <97915345+MJMenni@users.noreply.github.com>
2022-04-08 21:13:25 -0400

    Marie-Jade Menni

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -4,49 +4,74 @@ import Slider from "./Slider";
 
 const Dropdown = () => {
   return (
-    <DropWrapper>
-      <SymptomsLabel>Symptoms</SymptomsLabel>
-      <SymptomLabel>Visual Snow</SymptomLabel>
-      <AdjustLabel>Size</AdjustLabel>
-      <Slider />
-      <AdjustLabel>Speed</AdjustLabel>
-      <Slider />
-      <AdjustLabel>Opacity</AdjustLabel>
-      <Slider />
-      <SymptomLabel>Tinnitus</SymptomLabel>
-      <AudioWrapper>
-        <ReactAudioPlayer src="assets/Roaring.mp3" controls id="player1" />
+    <DropWrap>
+      <SymptomsTitle>
+        Adjust the settings to recreate your experience with VSS. To keep track
+        of your symptoms, log in at the bottom of the page and download your
+        stats.{" "}
+      </SymptomsTitle>
+
+      <SymptomsLabel>Visual Snow</SymptomsLabel>
+
+      <SliderWrap>
+        <AdjustLabel>Size</AdjustLabel>
+        <Slider />
+      </SliderWrap>
+
+      <SliderWrap>
+        <AdjustLabel>Speed</AdjustLabel>
+        <Slider />
+      </SliderWrap>
+
+      <SliderWrap>
+        <AdjustLabel>Opacity</AdjustLabel>
+        <Slider />
+      </SliderWrap>
+
+      <SymptomsLabel>Tinnitus</SymptomsLabel>
+
+      <AudioWrap>
         <TinnitusType>Roaring</TinnitusType>
-      </AudioWrapper>
-      <AudioWrapper>
-        <ReactAudioPlayer src="assets/TeaKettle.mp3" controls id="player1" />
+        <ReactAudioPlayer src="assets/Roaring.mp3" controls id="player1" loop />
+      </AudioWrap>
+      <AudioWrap>
         <TinnitusType>Tea kettle</TinnitusType>
-      </AudioWrapper>
-      <AudioWrapper>
-        <ReactAudioPlayer src="assets/Static.mp3" controls id="player1" />
+        <ReactAudioPlayer
+          src="assets/TeaKettle.mp3"
+          controls
+          id="player1"
+          loop
+        />
+      </AudioWrap>
+      <AudioWrap>
         <TinnitusType>Static</TinnitusType>
-      </AudioWrapper>
-      <AudioWrapper>
-        <ReactAudioPlayer src="assets/Screeching.mp3" controls id="player1" />
+        <ReactAudioPlayer src="assets/Static.mp3" controls id="player1" loop />
+      </AudioWrap>
+      <AudioWrap>
         <TinnitusType>Screeching</TinnitusType>
-      </AudioWrapper>
-      <AudioWrapper>
-        <ReactAudioPlayer src="assets/Electric.mp3" controls id="player1" />
+        <ReactAudioPlayer
+          src="assets/Screeching.mp3"
+          controls
+          id="player1"
+          loop
+        />
+      </AudioWrap>
+      <AudioWrap>
         <TinnitusType>Electric</TinnitusType>
-      </AudioWrapper>
-    </DropWrapper>
+        <AudioTrackWrap>
+          <ReactAudioPlayer
+            src="assets/Electric.mp3"
+            controls
+            id="player1"
+            loop
+          />
+        </AudioTrackWrap>
+      </AudioWrap>
+    </DropWrap>
   );
 };
 
-const TinnitusType = styled.div`
-  margin-left: -120px;
-  margin-top: 15px;
-  text-align: right;
-  z-index: 1;
-  font-weight: 900;
-`;
-
-const AudioWrapper = styled.div`
+const AudioWrap = styled.div`
   margin-top: 20px;
   display: flex;
   audio::-webkit-media-controls-current-time-display,
@@ -57,30 +82,58 @@ const AudioWrapper = styled.div`
   video::-webkit-media-controls-timeline {
     display: none;
   }
-`;
 
-const DropWrapper = styled.div`
-  margin-right: auto;
+  justify-content: space-between;
+  width: 65%;
   margin-left: auto;
-  /* max-width: 65%; */
-  width: 640px;
-  line-height: 24px;
-  margin-top: 400px;
+  margin-right: auto;
+  /* border: 1px green solid; */
 `;
-
-const SymptomsLabel = styled.div`
-  font-size: 20px;
+const SymptomsTitle = styled.div`
+  font-size: 18px;
+  text-align: justify;
   font-weight: 900;
   margin-top: 20px;
 `;
 
-const SymptomLabel = styled.div`
-  font-size: 18px;
-  font-weight: 900;
-  margin: 20px 0px 10px 0px;
+const AudioTrackWrap = styled.div`
+  align-items: flex-end;
+  /* border: 1px green solid; */
 `;
 
-const AdjustLabel = styled.div``;
+const TinnitusType = styled.div`
+  margin-top: 15px;
+  text-align: right;
+  z-index: 1;
+  font-weight: 900;
+  /* margin-left: -120px; */
+`;
+
+const DropWrap = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+  width: 640px;
+  line-height: 24px;
+  margin-top: 400px;
+  /* max-width: 65%; */
+`;
+
+const SliderWrap = styled.div`
+  display: flex;
+  margin-top: 20px;
+  justify-content: center;
+  /* border: 1px green solid; */
+`;
+
+const SymptomsLabel = styled.div`
+  font-size: 18px;
+  font-weight: 900;
+  margin: 50px 0px 25px 0px;
+`;
+
+const AdjustLabel = styled.div`
+  width: 80px;
+`;
 
 export default Dropdown;
 
--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -37,7 +37,7 @@ const SliderWrapper = styled.div`
 const Input = styled.input`
   -webkit-appearance: none;
   height: 7px;
-  width: 200px;
+  width: 300px;
   background: var(--blue);
   border-radius: 5px;
   background-repeat: no-repeat;
@@ -45,21 +45,7 @@ const Input = styled.input`
 `;
 
 const CurrentValue = styled.div`
-  margin-left: 10px;
+  margin-left: 25px;
 `;
 
 export default Slider;
-
-// Logic for prev slider
-// const MAX = 10;
-
-// Logic for prev slider
-//   const setAdjustment = () => {
-//     return { backgroundSize: `${(value * 100) / MAX}% 100%` };
-//   };
-
-// Settings for prev slider
-// max="{MAX}"
-// onChange={(e) => setValue(e.target.value)}
-// value={value}
-// style={setAdjustment()}
