160323c2d664d2aca334894a49b83e59e6a76731
Marie-Jade Menni <97915345+MJMenni@users.noreply.github.com>
2022-04-10 15:11:27 -0400

    Marie-Jade Menni

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,31 +1,29 @@
 import styled from "styled-components";
 import React from "react";
-import ReactPlayer from "react-player";
 
 const Video = () => {
   return (
     <Wrapper>
       <MainVideoWrapper>
-        <ReactPlayer
-          url="https://www.youtube.com/embed/ProM7N7v0cE?rel=0"
-          loop
-          controls="false"
+        <video
           width="960px"
           height="540px"
-          // playing="true"
-        />
+          controls
+          loop="true"
+          // autoplay="true"
+        >
+          <source src="/assets/Original.mp4" type="video/mp4"></source>
+        </video>
       </MainVideoWrapper>
       <OverlayVideoWrapper>
-        <ReactPlayer
-          url="https://www.youtube.com/embed/UCfKKW65q7Y?rel=0"
-          loop
-          controls="false"
+        <video
           width="960px"
           height="540px"
-          // width="1152px"
-          // height="648px"
-          // playing="true"
-        />
+          loop="true"
+          // autoplay="true"
+        >
+          <source src="/assets/Overlay.mp4" type="video/mp4"></source>
+        </video>
       </OverlayVideoWrapper>
     </Wrapper>
   );
@@ -65,20 +63,6 @@ export default Video;
 // let form = document.querySelector("#form");
 // let video = document.querySelector("#video");
 
-//Slider (former attempt)
-// const Label = styled.div`
-//   display: block;
-//   font-weight: bold;
-// `;
-
-// const Form = styled.form`
-//   font-size: 2em;
-//   background: #ccc;
-//   padding: 20px;
-//   max-width: 500px;
-//   margin: 20px auto;
-// `;
-
 // range.addEventListener("input", function () {
 //   form.style["font-size"] = this.value + "em";
 // });
@@ -102,38 +86,4 @@ export default Video;
     <Label for="">Label</Label>
     </Form>
     </Slider> */
-}
-
-//Prev video player
-{
-  /* <VideoContainer className="plyr__video-embed" id="player">
-          <MainVideoWrapper>
-            <iframe
-              width="640"
-              height="480"
-              src="https://www.youtube.com/embed/ProM7N7v0cE?rel=0"
-              title="YouTube video player"
-              frameborder="0"
-              modestbranding="0"
-              allow="autoplay; clipboard-write; encrypted-media; gyroscope; transparency; picture-in-picture; loop; autopause=0"
-              allowfullscreen
-              className="userVideo"
-            ></iframe>
-          </MainVideoWrapper>
-  
-          <OverlayVideoWrapper className="plyr__video-embed" id="video">
-            <iframe
-              id="overlay"
-              width="640"
-              height="480"
-              modestbranding="0"
-              src="https://www.youtube.com/embed/UCfKKW65q7Y?rel=0"
-              title="YouTube video player"
-              frameborder="0"
-              allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; loop; autopause=0"
-              allowfullscreen
-              className="peerVideo"
-            ></iframe>
-          </OverlayVideoWrapper>
-        </VideoContainer> */
 }
