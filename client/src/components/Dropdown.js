a1d9894f9c88d8383b45cb1e8f551a8b663236fd
Marie-Jade Menni <97915345+MJMenni@users.noreply.github.com>
2022-04-13 22:44:39 -0400

    Marie-Jade Menni

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,212 +1,213 @@
-import styled from "styled-components";
-import { useState, useEffect } from "react";
+// import styled from "styled-components";
+// import { useState, useEffect } from "react";
 
-const Dropdown = ({ audio, setAudio }) => {
-  const initValue = {
-    Static: false,
-    Roaring: false,
-    Buzzing: false,
-    Kettle: false,
-    Electric: false,
-    Screeching: false,
-  };
-  const [stats, setStats] = useState(initValue);
+// const Dropdown = () => {
+//   // const Dropdown = ({ audio, setAudio }) => {
+//   const initValue = {
+//     Static: false,
+//     Roaring: false,
+//     Buzzing: false,
+//     Kettle: false,
+//     Electric: false,
+//     Screeching: false,
+//   };
+//   const [audio, setAudio] = useState(initValue);
 
-  console.log(stats);
-  const onClickHandler = (event) => {
-    //click triggered
-    event.preventDefault();
+//   console.log(audio);
+//   const onClickHandler = (event) => {
+//     //click triggered
+//     event.preventDefault();
 
-    //change the stat target value
-    console.log(event.target.id);
-    const key = event.target.id;
-    setStats((prev) => {
-      return { ...prev, [key]: !prev[key] };
-    });
-  };
+//     //change the stat target value
+//     console.log(event.target.id);
+//     const key = event.target.id;
+//     setAudio((prev) => {
+//       return { ...prev, [key]: !prev[key] };
+//     });
+//   };
 
-  useEffect(() => {
-    console.log(stats);
-  }, [stats]);
+//   useEffect(() => {
+//     console.log(audio);
+//   }, [audio]);
 
-  return (
-    <Wrap>
-      <InnerWrap>
-        <SymptomsLabel>Tinnitus</SymptomsLabel>
-        <AudioOuterWrap>
-          <TinnitusType>Static</TinnitusType>
-          <AudioWrap>
-            <audio
-              src="assets/Static.mp3"
-              id="Static"
-              controls
-              loop
-              onPlay={(e) => {
-                onClickHandler(e);
-              }}
-              onPause={(e) => {
-                onClickHandler(e);
-              }}
-            />
-          </AudioWrap>
+//   return (
+//     <Wrap>
+//       <InnerWrap>
+//         <SymptomsLabel>Tinnitus</SymptomsLabel>
+//         <AudioOuterWrap>
+//           <TinnitusType>Static</TinnitusType>
+//           <AudioWrap>
+//             <audio
+//               src="assets/Static.mp3"
+//               id="Static"
+//               controls
+//               loop
+//               onPlay={(e) => {
+//                 onClickHandler(e);
+//               }}
+//               onPause={(e) => {
+//                 onClickHandler(e);
+//               }}
+//             />
+//           </AudioWrap>
 
-          <TinnitusType>Kettle</TinnitusType>
-          <AudioWrap>
-            <audio
-              src="assets/Kettle.mp3"
-              controls
-              id="Kettle"
-              loop
-              onPlay={(e) => {
-                onClickHandler(e);
-              }}
-              onPause={(e) => {
-                onClickHandler(e);
-              }}
-            />
-          </AudioWrap>
-          <TinnitusType>Roaring</TinnitusType>
-          <AudioWrap>
-            <audio
-              src="assets/Roaring.mp3"
-              controls
-              id="Roaring"
-              loop
-              onPlay={(e) => {
-                onClickHandler(e);
-              }}
-              onPause={(e) => {
-                onClickHandler(e);
-              }}
-            />
-          </AudioWrap>
-          <TinnitusType>Electric</TinnitusType>
-          <AudioWrap>
-            <audio
-              src="assets/Electric.mp3"
-              controls
-              id="Electric"
-              loop
-              onPlay={(e) => {
-                onClickHandler(e);
-              }}
-              onPause={(e) => {
-                onClickHandler(e);
-              }}
-            />
-          </AudioWrap>
-          <TinnitusType>Buzzing</TinnitusType>
-          <AudioWrap>
-            <audio
-              src="assets/Buzzing.mp3"
-              controls
-              id="Buzzing"
-              loop
-              onPlay={(e) => {
-                onClickHandler(e);
-              }}
-              onPause={(e) => {
-                onClickHandler(e);
-              }}
-            />
-          </AudioWrap>
-          <TinnitusType>Screeching</TinnitusType>
-          <AudioWrap>
-            <audio
-              src="assets/Screeching.mp3"
-              controls
-              id="Screeching"
-              loop
-              onPlay={(e) => {
-                onClickHandler(e);
-              }}
-              onPause={(e) => {
-                onClickHandler(e);
-              }}
-            />
-          </AudioWrap>
-        </AudioOuterWrap>
-        <SymptomsLabel>Notes</SymptomsLabel>
-        <NotesInput
-          placeholder="Feel free to add notes here. For example: &#10;Apr-20-2022 – Intense after-images and light sensitivity, especially when walking through the mall. Noticed static, tinnitus, and floaters increased as the day went on. Possible triggers: work-related stress, exposure to intense lighting, too much screen time."
-        ></NotesInput>
-      </InnerWrap>
-    </Wrap>
-  );
-};
+//           <TinnitusType>Kettle</TinnitusType>
+//           <AudioWrap>
+//             <audio
+//               src="assets/Kettle.mp3"
+//               controls
+//               id="Kettle"
+//               loop
+//               onPlay={(e) => {
+//                 onClickHandler(e);
+//               }}
+//               onPause={(e) => {
+//                 onClickHandler(e);
+//               }}
+//             />
+//           </AudioWrap>
+//           <TinnitusType>Roaring</TinnitusType>
+//           <AudioWrap>
+//             <audio
+//               src="assets/Roaring.mp3"
+//               controls
+//               id="Roaring"
+//               loop
+//               onPlay={(e) => {
+//                 onClickHandler(e);
+//               }}
+//               onPause={(e) => {
+//                 onClickHandler(e);
+//               }}
+//             />
+//           </AudioWrap>
+//           <TinnitusType>Electric</TinnitusType>
+//           <AudioWrap>
+//             <audio
+//               src="assets/Electric.mp3"
+//               controls
+//               id="Electric"
+//               loop
+//               onPlay={(e) => {
+//                 onClickHandler(e);
+//               }}
+//               onPause={(e) => {
+//                 onClickHandler(e);
+//               }}
+//             />
+//           </AudioWrap>
+//           <TinnitusType>Buzzing</TinnitusType>
+//           <AudioWrap>
+//             <audio
+//               src="assets/Buzzing.mp3"
+//               controls
+//               id="Buzzing"
+//               loop
+//               onPlay={(e) => {
+//                 onClickHandler(e);
+//               }}
+//               onPause={(e) => {
+//                 onClickHandler(e);
+//               }}
+//             />
+//           </AudioWrap>
+//           <TinnitusType>Screeching</TinnitusType>
+//           <AudioWrap>
+//             <audio
+//               src="assets/Screeching.mp3"
+//               controls
+//               id="Screeching"
+//               loop
+//               onPlay={(e) => {
+//                 onClickHandler(e);
+//               }}
+//               onPause={(e) => {
+//                 onClickHandler(e);
+//               }}
+//             />
+//           </AudioWrap>
+//         </AudioOuterWrap>
+//         <SymptomsLabel>Notes</SymptomsLabel>
+//         <NotesInput
+//           placeholder="Feel free to add notes here. For example: &#10;Apr-20-2022 – Intense after-images and light sensitivity, especially when walking through the mall. Noticed static, tinnitus, and floaters increased as the day went on. Possible triggers: work-related stress, exposure to intense lighting, too much screen time."
+//         ></NotesInput>
+//       </InnerWrap>
+//     </Wrap>
+//   );
+// };
 
-// Wraps
-const Wrap = styled.div`
-  display: flex;
-  max-width: 956px;
-  margin-right: auto;
-  margin-left: auto;
-`;
+// // Wraps
+// const Wrap = styled.div`
+//   display: flex;
+//   max-width: 956px;
+//   margin-right: auto;
+//   margin-left: auto;
+// `;
 
-const InnerWrap = styled.div`
-  width: 85%;
-  margin-right: auto;
-  margin-left: auto;
-  display: flex;
-  flex-direction: column;
-  line-height: 24px;
-`;
+// const InnerWrap = styled.div`
+//   width: 85%;
+//   margin-right: auto;
+//   margin-left: auto;
+//   display: flex;
+//   flex-direction: column;
+//   line-height: 24px;
+// `;
 
-// Symptoms label
-const SymptomsLabel = styled.div`
-  font-size: 18px;
-  font-weight: 900;
-  margin: 40px 0px 25px 0px;
-`;
+// // Symptoms label
+// const SymptomsLabel = styled.div`
+//   font-size: 18px;
+//   font-weight: 900;
+//   margin: 40px 0px 25px 0px;
+// `;
 
-// Tinnitus section
-const TinnitusType = styled.div`
-  margin-top: 25px;
-  font-weight: 900;
-  width: 15%;
-  height: 15%;
-`;
+// // Tinnitus section
+// const TinnitusType = styled.div`
+//   margin-top: 25px;
+//   font-weight: 900;
+//   width: 15%;
+//   height: 15%;
+// `;
 
-const AudioOuterWrap = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  width: 85%;
-  background-color: #f1f3f4;
-  padding: 15px 5px 15px 35px;
-  margin-right: auto;
-  margin-left: auto;
-  border-radius: 5px;
-`;
+// const AudioOuterWrap = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   flex-wrap: wrap;
+//   width: 85%;
+//   background-color: #f1f3f4;
+//   padding: 15px 5px 15px 35px;
+//   margin-right: auto;
+//   margin-left: auto;
+//   border-radius: 5px;
+// `;
 
-const AudioWrap = styled.div`
-  margin: 10px;
-  display: flex;
-  width: 30%;
-  margin-left: auto;
-  margin-right: auto;
+// const AudioWrap = styled.div`
+//   margin: 10px;
+//   display: flex;
+//   width: 30%;
+//   margin-left: auto;
+//   margin-right: auto;
 
-  audio::-webkit-media-controls-current-time-display,
-  audio::-webkit-media-controls-time-remaining-display {
-    display: none;
-  }
-  audio::-webkit-media-controls-timeline,
-  video::-webkit-media-controls-timeline {
-    display: none;
-  }
-`;
+//   audio::-webkit-media-controls-current-time-display,
+//   audio::-webkit-media-controls-time-remaining-display {
+//     display: none;
+//   }
+//   audio::-webkit-media-controls-timeline,
+//   video::-webkit-media-controls-timeline {
+//     display: none;
+//   }
+// `;
 
-// Notes section
-const NotesInput = styled.textarea`
-  border: var(--blue) 1px solid;
-  min-height: 175px;
-  width: 85%;
-  margin-right: auto;
-  margin-left: auto;
-  padding: 25px;
-  border-radius: 5px;
-  line-height: 24px;
-  font-size: 16px;
-`;
+// // Notes section
+// const NotesInput = styled.textarea`
+//   border: var(--blue) 1px solid;
+//   min-height: 175px;
+//   width: 85%;
+//   margin-right: auto;
+//   margin-left: auto;
+//   padding: 25px;
+//   border-radius: 5px;
+//   line-height: 24px;
+//   font-size: 16px;
+// `;
 
-export default Dropdown;
+// export default Dropdown;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
-import Dropdown from "./Dropdown";
 import Video from "./Video";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -26,9 +25,34 @@ const Home = () => {
 
   //stats status
   const [slider, setSlider] = useState(initialState);
+  console.log(slider);
+
   //audio
+  const initValue = {
+    Static: false,
+    Roaring: false,
+    Buzzing: false,
+    Kettle: false,
+    Electric: false,
+    Screeching: false,
+  };
+  const [audio, setAudio] = useState(initValue);
+
+  console.log(audio);
+  const onClickHandler = (event) => {
+    //click triggered
+    event.preventDefault();
+
+    //change the stat target value
+    console.log(event.target.id);
+    const key = event.target.id;
+    setAudio((prev) => {
+      return { ...prev, [key]: !prev[key] };
+    });
+  };
 
   //note
+  const [note, setNote] = useState("");
 
   return (
     <Wrap>
@@ -116,7 +140,111 @@ const Home = () => {
           </Slider>
         </SliderWrap>
       </SliderOuterWrap>
-      <Dropdown />
+
+      <WrapAudio>
+        <InnerWrap>
+          <SymptomsLabel>Tinnitus</SymptomsLabel>
+          <AudioOuterWrap>
+            <TinnitusType>Static</TinnitusType>
+            <AudioWrap>
+              <audio
+                src="assets/Static.mp3"
+                id="Static"
+                controls
+                loop
+                onPlay={(e) => {
+                  onClickHandler(e);
+                }}
+                onPause={(e) => {
+                  onClickHandler(e);
+                }}
+              />
+            </AudioWrap>
+
+            <TinnitusType>Kettle</TinnitusType>
+            <AudioWrap>
+              <audio
+                src="assets/Kettle.mp3"
+                controls
+                id="Kettle"
+                loop
+                onPlay={(e) => {
+                  onClickHandler(e);
+                }}
+                onPause={(e) => {
+                  onClickHandler(e);
+                }}
+              />
+            </AudioWrap>
+            <TinnitusType>Roaring</TinnitusType>
+            <AudioWrap>
+              <audio
+                src="assets/Roaring.mp3"
+                controls
+                id="Roaring"
+                loop
+                onPlay={(e) => {
+                  onClickHandler(e);
+                }}
+                onPause={(e) => {
+                  onClickHandler(e);
+                }}
+              />
+            </AudioWrap>
+            <TinnitusType>Electric</TinnitusType>
+            <AudioWrap>
+              <audio
+                src="assets/Electric.mp3"
+                controls
+                id="Electric"
+                loop
+                onPlay={(e) => {
+                  onClickHandler(e);
+                }}
+                onPause={(e) => {
+                  onClickHandler(e);
+                }}
+              />
+            </AudioWrap>
+            <TinnitusType>Buzzing</TinnitusType>
+            <AudioWrap>
+              <audio
+                src="assets/Buzzing.mp3"
+                controls
+                id="Buzzing"
+                loop
+                onPlay={(e) => {
+                  onClickHandler(e);
+                }}
+                onPause={(e) => {
+                  onClickHandler(e);
+                }}
+              />
+            </AudioWrap>
+            <TinnitusType>Screeching</TinnitusType>
+            <AudioWrap>
+              <audio
+                src="assets/Screeching.mp3"
+                controls
+                id="Screeching"
+                loop
+                onPlay={(e) => {
+                  onClickHandler(e);
+                }}
+                onPause={(e) => {
+                  onClickHandler(e);
+                }}
+              />
+            </AudioWrap>
+          </AudioOuterWrap>
+          <SymptomsLabel>Notes</SymptomsLabel>
+        </InnerWrap>
+      </WrapAudio>
+      <NotesWrap>
+        <NotesInput
+          placeholder="Feel free to add notes here. For example: &#10;Apr-20-2022 – Intense after-images and light sensitivity, especially when walking through the mall. Noticed static, tinnitus, and floaters increased as the day went on. Possible triggers: work-related stress, exposure to intense lighting, too much screen time."
+        ></NotesInput>
+      </NotesWrap>
       <SectionTitle>Login</SectionTitle>
       <Signup />
       <Login />
@@ -237,4 +365,85 @@ const CurrentValue = styled.div`
   margin-right: 10px;
 `;
 
+//From dropdown
+// Wraps
+const WrapAudio = styled.div`
+  display: flex;
+  max-width: 956px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const InnerWrap = styled.div`
+  width: 85%;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  line-height: 24px;
+`;
+
+// Symptoms label
+const SymptomsLabel = styled.div`
+  font-size: 18px;
+  font-weight: 900;
+  margin: 40px 0px 25px 0px;
+`;
+
+// Tinnitus section
+const TinnitusType = styled.div`
+  margin-top: 25px;
+  font-weight: 900;
+  width: 15%;
+  height: 15%;
+`;
+
+const AudioOuterWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 85%;
+  background-color: #f1f3f4;
+  padding: 15px 5px 15px 35px;
+  margin-right: auto;
+  margin-left: auto;
+  border-radius: 5px;
+`;
+
+const AudioWrap = styled.div`
+  margin: 10px;
+  display: flex;
+  width: 30%;
+  margin-left: auto;
+  margin-right: auto;
+
+  audio::-webkit-media-controls-current-time-display,
+  audio::-webkit-media-controls-time-remaining-display {
+    display: none;
+  }
+  audio::-webkit-media-controls-timeline,
+  video::-webkit-media-controls-timeline {
+    display: none;
+  }
+`;
+
+// Notes section
+const NotesWrap = styled.div`
+  display: flex;
+  max-width: 956px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const NotesInput = styled.textarea`
+  border: var(--blue) 1px solid;
+  min-height: 150px;
+  width: 72%;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 25px;
+  border-radius: 5px;
+  line-height: 24px;
+  font-size: 16px;
+`;
 export default Home;
